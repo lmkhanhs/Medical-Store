601ba68e0a6c20b3f0531c2585adeaca010025f9
Lê Minh Khánh <khanhlm250204@gmail.com>
2025-11-15 16:02:44 +0700

    update AI code

--- AI/app/main.py
+++ AI/app/main.py
@@ -11,3 +11,12 @@ def index():
 
 # VENV\Scripts\activate
 # uvicorn app.main:app --reload
+
+if __name__ == "__main__":
+    import uvicorn 
+    uvicorn.run(
+        "app.main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True
+    )
